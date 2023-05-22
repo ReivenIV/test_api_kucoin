b61146ddb7cc841c91e185622194f9344f8117ca
reivenIV <runyalen88@duck.com>
2023-05-22 09:17:23 +0200

    name variables changes on delta/functions.js

--- src/routes/data/delta/functions.ts
+++ src/routes/data/delta/functions.ts
@@ -19,19 +19,19 @@ export async function calculateDelta(pair1: string, pair2: string) {
   let data = await fetch_market(pair1, pair2);
 
   let value = data.data;
-  let all_buy: number = 0;
-  let all_sell: number = 0;
+  let allBuy: number = 0;
+  let allSell: number = 0;
   for (let i = 0; i < value.length; i++) {
-    let type_of_transaction = value[i].side;
-    let amount_value = value[i].size;
+    let typeOfTransaction = value[i].side;
+    let amountValue = value[i].size;
 
-    if (type_of_transaction === 'buy') {
-      all_buy += parseFloat(amount_value);
+    if (typeOfTransaction === 'buy') {
+      allBuy += parseFloat(amountValue);
     } else {
-      all_sell += parseFloat(amount_value);
+      allSell += parseFloat(amountValue);
     }
   }
-  let delta: number = all_buy - all_sell;
+  let delta: number = allBuy - allSell;
 
   return delta;
 }
